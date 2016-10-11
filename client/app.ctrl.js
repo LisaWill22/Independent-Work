3dd237f49ad844f261b276e6ae5c39e94c267cf5
brentoneill <brentoneill@gmail.com>
2016-10-10 21:36:04 -0600

    Added some build tasks, updated navbar and app conatiner styles

--- client/app.ctrl.js
+++ client/app.ctrl.js
@@ -1,4 +1,18 @@
 angular.module('independent-work-app')
     .controller('AppCtrl', function($scope) {
         console.log('main app ctrl loaded >> ', $scope);
-    });
+
+
+    })
+    .directive('slimHeader', function ($window) {
+        return function(scope, element, attrs) {
+            angular.element($window).bind("scroll", function() {
+                 if (this.pageYOffset >= 50) {
+                     scope.slimHeader = true;
+                 } else {
+                     scope.slimHeader = false;
+                 }
+                scope.$apply();
+            });
+        };
+});
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,20 @@ var gulp    = require('gulp'),
     concat  = require('gulp-concat'),
     minify  = require('gulp-minify-css'),
     watch   = require('gulp-watch'),
+    jshint  = require('gulp-jshint'),
     del     = require('del'),
     nodemon = require('gulp-nodemon');
+
+var browserSync = require('browser-sync').create();
+var runSequence = require('gulp-run-sequence');
+
+var PATHS = {
+    LESS: './client/stylesheets/less/**/*.less',
+    LESSIMPORT: './client/stylesheets/less/imports.less',
+    CSS: './client/stylesheets',
+    JS: './client/**/*.js',
+    BOWER_COMPONENTS: './client/bower_components'
+}
 
 gulp.task('install', function(){
     return gulp.src(['./bower.json', './package.json'])
@@ -16,22 +28,47 @@ gulp.task('clean', function(){
     del(['./client/stylesheets/style.css']);
 });
 
-gulp.task('less', function(){
-    return gulp.src('./client/stylesheets/less/imports.less')
+gulp.task('js', function() {
+    return gulp.src([PATHS.JS, '!' + PATHS.BOWER_COMPONENTS])
+            .pipe(jshint('.jshintrc'))
+            .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: 'localhost:3000'
+    });
+});
+
+gulp.task('less', function() {
+    return gulp.src(PATHS.LESSIMPORT)
             .pipe(less())
             .pipe(concat('style.css'))
             .pipe(minify())
             .pipe(gulp.dest('./client/stylesheets'));
 });
 
-gulp.task('default', ['less'], function(){
-    var watcher = gulp.watch('./client/stylesheets/less/**/*.less', ['less']);
+gulp.task('reload', function() {
+    browserSync.reload();
+});
+
+gulp.task('dev', function() {
+    runSequence(
+        'less',
+        'js',
+        'browser-sync'
+    );
+});
+
+gulp.task('default', ['dev'], function(){
+    var watcher = gulp.watch(PATHS.LESS, ['less']);
+    gulp.watch(PATHS.JS, ['js'])
     nodemon({
         script: 'server.js',
-        ext: 'js html',
+        ext: 'js',
         env: { 'NODE_ENV': 'development' }
     });
-    watcher.on('change', function(event){
+    watcher.on('chdange', function(event){
         console.log('File ' + event.path + ' was ' + event.type);
     });
 });
