3074932f7cdaf12efc5c439aeeabe2d27257dde9
brentoneill <brentoneill@gmail.com>
2016-11-24 14:08:18 -0700

    More style fixes

--- client/js/app.js
+++ client/js/app.js
@@ -74,6 +74,22 @@ app.run(function($timeout, $rootScope) {
 	    $('#home-loading').remove();
 	}, 1501);
 
+	// TODO: Move this to a factory
+	// Init WOW for animations on scroll
+	var wow = new WOW({
+		boxClass: 'wow', // animated element css class (default is wow)
+		animateClass: 'animated', // animation css class (default is animated)
+		offset: 0, // distance to the element when triggering the animation (default is 0)
+		mobile: true, // trigger animations on mobile devices (default is true)
+		live: true, // act on asynchronously loaded content (default is true)
+		callback: function(box) {
+			// the callback is fired every time an animation is started
+			// the argument that is passed in is the DOM node being animated
+		},
+		scrollContainer: null // optional scroll container selector, otherwise use window
+	});
+	wow.init();
+
 });
 
 //Service to interact with the socket library
