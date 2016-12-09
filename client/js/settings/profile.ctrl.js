deea4417750a488bbb4cb453e75a0a4a1edbfe00
brentoneill <brentoneill@gmail.com>
2016-12-08 19:29:46 -0700

    Fixed skill add/select on profile form and create/edit post form

--- client/js/settings/profile.ctrl.js
+++ client/js/settings/profile.ctrl.js
@@ -1,150 +1,177 @@
 'use strict';
 
 angular.module('settings')
-    .controller('ProfileCtrl', function($scope, $rootScope, $http, $q, $timeout, toastr, Upload, states) {
+	.controller('ProfileCtrl', function($scope, $rootScope, $http, $q, $timeout, toastr, Upload, states) {
 
-        console.log('ProfileCtrl loaded >>', $scope);
+		console.log('ProfileCtrl loaded >>', $scope);
 
-        $scope.data = angular.copy($scope.currentUser);
-        $scope.data.showEmail = 'true';
-        $scope.states = states;
+		$scope.data = angular.copy($scope.currentUser);
+		$scope.data.showEmail = 'true';
+		$scope.states = states;
 
-        // Placeholder for the list of all skills returned from the api
-        var allSkills;
+		// Placeholder for the list of all skills returned from the api
+		var allSkills;
 
-        if ($scope.contractor) {
-            $http.get('/api/skills')
-                .then(function(res){
-                    allSkills = res.data;
-                    $scope.skills = [];
-                    allSkills.forEach(function(skill) {
-                        var userHasSkill = $scope.data.skills.find(function(userSkill) {
-                            return userSkill.name === skill.name;
-                        });
-                        if (!userHasSkill) {
-                            $scope.skills.push(skill);
-                        }
-                    });
-                })
-                .catch(function(err) {
-                    console.log(err);
-                    $scope.skills = [];
-                })
-                .finally(function() {
+		if ($scope.contractor) {
+			$http.get('/api/skills')
+				.then(function(res) {
+					allSkills = res.data;
+					$scope.skills = [];
+					allSkills.forEach(function(skill) {
+						var userHasSkill = $scope.data.skills.find(function(userSkill) {
+							return userSkill.name === skill.name;
+						});
+						if (!userHasSkill) {
+							$scope.skills.push(skill);
+						}
+					});
+				})
+				.catch(function(err) {
+					console.log(err);
+					$scope.skills = [];
+				});
+		}
 
-                });
-        }
-        //
+		// Saves a skill to the api - called in profile save to save new skills
+		function saveSkill(skill) {
+			return $http.post('/api/skills', skill);
+		}
 
-        // Saves a skill to the api - called in profile save to save new skills
-        function saveSkill (skill) {
-            return $http.post('/api/skills', skill);
-        }
+        $scope.skills = $scope.skills.map(function(skill) {
+            return {
+                id: skill._id,
+                title: skill.name
+            };
+        });
 
-        $scope.onError = function () {
-            toastr.warning('Whoops, something went wrong...\n ' + err);
-        };
+		$scope.myOptions = [{
+			id: 1,
+			title: 'Spectrometer'
+		}, {
+			id: 2,
+			title: 'Star Chart'
+		}, {
+			id: 3,
+			title: 'Laser Pointer'
+		}];
 
-        // Adds back skill to list of options on removal
-        $scope.onSkillRemove = function(item, model) {
-            if ($scope.skills.indexOf(item) === -1) {
-                $scope.skills.push(item);
-            }
-        };
+		$scope.myConfig = {
+			create: true,
+			valueField: 'id',
+			labelField: 'title',
+			delimiter: '|',
+			placeholder: 'Pick something',
+			onInitialize: function(selectize) {
+				// receives the selectize object as an argument
+			},
+			// maxItems: 1
+		};
 
-        // Adds a skill tag when it does not exist yet
-        $scope.addSkill = function(skillName) {
+		$scope.onError = function() {
+			toastr.warning('Whoops, something went wrong...\n ' + err);
+		};
 
-            var newSkill;
-            var skillNameExists = $scope.skills.find(function(skill) {
-                return skillName.toLowerCase() === skill.name.toLowerCase();
-            });
+		// Adds back skill to list of options on removal
+		$scope.onSkillRemove = function(item, model) {
+			if ($scope.skills.indexOf(item) === -1) {
+				$scope.skills.push(item);
+			}
+		};
 
-            // Prevent the skill from adding if it already exists
-            if (!skillNameExists) {
-                newSkill = {
-                    name: skillName,
-                    description: null,
-                    cateogries: [],
-                    _created: new Date()
-                };
-            } else {
-                newSkill = null;
-            }
+		// Adds a skill tag when it does not exist yet
+		$scope.addSkill = function(skillName) {
 
-            return newSkill;
-        };
+			var newSkill;
 
-        $scope.beforeSubmit = function() {
-            delete $scope.data.image;
-            $scope.data.messages = {};
+			var skillNameExists = $scope.skills.find(function(skill) {
+				return skillName.toLowerCase() === skill.name.toLowerCase();
+			});
 
-            if ($scope.contractor) {
-                var skillsToSave = [];
-                var promises = [];
+			// Prevent the skill from adding if it already exists
+			if (!skillNameExists) {
+				newSkill = {
+					name: skillName,
+					description: null,
+					cateogries: [],
+					_created: new Date()
+				};
+			} else {
+				newSkill = null;
+			}
 
-                $scope.data.skills.forEach(function(skill) {
-                    var skillExists = allSkills.find(function(existingSkill) {
-                        return existingSkill.name.toLowerCase() === skill.name.toLowerCase();
-                    })
-                    if (!skillExists) {
-                        skillsToSave.push(skill);
-                    }
-                });
+			return newSkill;
+		};
 
-                skillsToSave.forEach(function(skill) {
-                    var promise = saveSkill(skill);
-                    promises.push(promise);
-                });
+		$scope.beforeSubmit = function() {
+			delete $scope.data.image;
+			$scope.data.messages = {};
 
-                $q.all(promises).then(function(allResults) {
-                    if (allResults.length) {
-                        console.log('skills uploaded > ', allResults);
-                    } else {
-                        console.log('no skills to up load :()');
-                    }
-                });
-            }
-        };
+			if ($scope.contractor) {
+				var skillsToSave = [];
+				var promises = [];
 
-        $scope.afterSubmit = function(res) {
-            $scope.$emit('Session:refresh', res.data.user);
-            toastr.success('Your profile saved successfully!');
-        };
+				$scope.data.skills.forEach(function(skill) {
+					var skillExists = allSkills.find(function(existingSkill) {
+						return existingSkill.name.toLowerCase() === skill.name.toLowerCase();
+					})
+					if (!skillExists) {
+						skillsToSave.push(skill);
+					}
+				});
 
-        // Runs when user uploads a profile picture, not when they click submit
-        // -- does not get submitted with "Save Profile"
-        $scope.upload = function (dataUrl, name) {
-            $scope.uploadInProgresss = true;
-            $scope.uploadFinished = false;
-            Upload.upload({
-                url: '/api/users/' + $scope.currentUser._id + '/profile-image',
-                data: {
-                    file: Upload.dataUrltoBlob(dataUrl, name)
-                },
-            }).then(function (response) {
-                $timeout(function () {
-                    $scope.uploadInProgresss = false;
-                    $scope.uploadFinished = true;
-                    $scope.result = response.data;
-                    $scope.$emit('Session:refresh', response.data.user);
-                    toastr.success('Your profile image was updated successfully');
-                    $scope.picFile = null;
-                });
+				skillsToSave.forEach(function(skill) {
+					var promise = saveSkill(skill);
+					promises.push(promise);
+				});
 
-                $timeout(function() {
-                    $scope.hideSuccess = true;
-                }, 2000);
-            }, function (response) {
-                if (response.status > 0) $scope.errorMsg = response.status
-                    + ': ' + response.data;
-            }, function (evt) {
-                $scope.progress = parseInt(100.0 * evt.loaded / evt.total);
-            })
-            .finally(function() {
-                $scope.uploadInProgresss = false;
-                $scope.uploadFinished = true;
-                $scope.picFile = null;
-            })
-        };
-    });
+				$q.all(promises).then(function(allResults) {
+					if (allResults.length) {
+						console.log('skills uploaded > ', allResults);
+					} else {
+						console.log('no skills to up load :()');
+					}
+				});
+			}
+		};
+
+		$scope.afterSubmit = function(res) {
+			$scope.$emit('Session:refresh', res.data.user);
+			toastr.success('Your profile saved successfully!');
+		};
+
+		// Runs when user uploads a profile picture, not when they click submit
+		// -- does not get submitted with "Save Profile"
+		$scope.upload = function(dataUrl, name) {
+			$scope.uploadInProgresss = true;
+			$scope.uploadFinished = false;
+			Upload.upload({
+					url: '/api/users/' + $scope.currentUser._id + '/profile-image',
+					data: {
+						file: Upload.dataUrltoBlob(dataUrl, name)
+					},
+				}).then(function(response) {
+					$timeout(function() {
+						$scope.uploadInProgresss = false;
+						$scope.uploadFinished = true;
+						$scope.result = response.data;
+						$scope.$emit('Session:refresh', response.data.user);
+						toastr.success('Your profile image was updated successfully');
+						$scope.picFile = null;
+					});
+
+					$timeout(function() {
+						$scope.hideSuccess = true;
+					}, 2000);
+				}, function(response) {
+					if (response.status > 0) $scope.errorMsg = response.status +
+						': ' + response.data;
+				}, function(evt) {
+					$scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+				})
+				.finally(function() {
+					$scope.uploadInProgresss = false;
+					$scope.uploadFinished = true;
+					$scope.picFile = null;
+				})
+		};
+	});
