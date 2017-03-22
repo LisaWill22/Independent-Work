d6a7d05bae59ffc2d0952316ad13d138c6d4aa1f
Gabriel Brickle <gabrielbrickle@gmail.com>
2017-03-22 19:06:57 -0400

    removed .env

--- server/config/es.js
+++ server/config/es.js
@@ -2,8 +2,8 @@
 const elasticsearch = require('elasticsearch');
 const esClient = new elasticsearch.Client({
 	//TODO REPLACE
-	// host: process.env.SEARCHBOX_SSL_URL,
-	host: "https://paas:8c733a71d679fa9b28f1f0ca26824c80@dori-us-east-1.searchly.com",
+	host: process.env.SEARCHBOX_SSL_URL,
+	// host: "https://paas:8c733a71d679fa9b28f1f0ca26824c80@dori-us-east-1.searchly.com",
 	log: 'trace'
 });
 
