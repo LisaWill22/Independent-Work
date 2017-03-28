1413c002d672329491cfd83f7d4a12b7f3756b2a
Gabriel Brickle <gabrielbrickle@gmail.com>
2017-03-28 13:58:27 -0400

    test

--- client/js/app.js
+++ client/js/app.js
@@ -88,7 +88,7 @@ app.config(function($stateProvider, $urlRouterProvider, $localStorageProvider, $
                 socketUrl: 'http://localhost:3000',
             },
             production: {
-                socketUrl: 'https://independentwork.com',
+                socketUrl: 'https://launchpeer-iw-herokuapp.com',
             }
         }
     });
